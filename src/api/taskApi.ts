9b3249951d0d55c788ef9f41c3bc3fddab8a70bd
slec16 <sankopytov.76@mail.ru>
2025-09-16 00:17:13 +0300

    feat: task api

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -4,15 +4,77 @@ export interface Task {
     description: string,
     priority: string,
     status: string,
-    board: string,
+    boardId: number,
+    assignee: {
+        id: number,
+        fullName: string,
+        email: string,
+        avatarUrl: string
+    }
     boardName: string
 }
 
-export const taskApi = {
-    
-    getTasks: async (): Promise<Task>  => {
+export interface createTask {
+    assigneeId: number,
+    boardId: number,
+    description: string,
+    priority: string,
+    title: string
+}
 
-        
-    }
+export interface updateTask {
+    assigneeId: number,
+    boardId: number,
+    description: string,
+    priority: string,
+    title: string
+}
+
+const baseUrl = 'http://localhost:8080'
+
+export const taskApi = {
+
+    getTasks: async (): Promise<Task[]> => {
+        const response = await fetch(`${baseUrl}/api/v1/tasks`)
+        if (!response.ok) throw new Error('Failed to fetch tasks')
+        return response.json();
+    },
+
+    getTask: async (id: number): Promise<Task> => {
+        const response = await fetch(`${baseUrl}/api/v1/tasks/${id}`)
+        if (!response.ok) throw new Error('Failed to fetch task')
+        return response.json()
+    },
+
+    createTask: async (taskData: createTask): Promise<{ id: number }> => {
+        const response = await fetch(`${baseUrl}/api/v1/tasks/create`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(taskData),
+        });
+        if (!response.ok) throw new Error('Failed to create task')
+        return response.json()
+    },
+
+    updateTask: async (id: number, taskData: updateTask): Promise<{message: string}> => {
+        const response = await fetch(`${baseUrl}/api/v1/tasks/update/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(taskData),
+        });
+        if (!response.ok) throw new Error('Failed to update task');
+        return response.json();
+    },
+
+    updateTaskStatus: async (id: number, taskStatus: {status: string}): Promise<{message: string}> => {
+        const response = await fetch(`${baseUrl}/api/v1/tasks/updateStatus/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(taskStatus),
+        });
+        if (!response.ok) throw new Error('Failed to update task');
+        return response.json();
+    },
+
 
 }
---
+++ src/hooks/useTasks.ts
@@ -0,0 +1,78 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { taskApi, type createTask, type updateTask } from '../api/taskApi'
+
+
+
+// Ключи для query cache
+export const taskKeys = {
+    all: ['tasks'] as const,
+    lists: () => [...taskKeys.all, 'list'] as const,
+    list: (filters: string) => [...taskKeys.lists(), { filters }] as const,
+    details: () => [...taskKeys.all, 'detail'] as const,
+    detail: (id: number) => [...taskKeys.details(), id] as const,
+}
+
+
+
+// Хук для получения всех задач
+export const useTasks = (filters?: string) => {
+    return useQuery({
+        queryKey: taskKeys.list(filters || ''),
+        queryFn: taskApi.getTasks,
+        staleTime: 1 * 60 * 1000, // 1 минута
+    })
+}
+
+// Хук для получения информации по задаче
+export const useTask = (id: number) => {
+    return useQuery({
+        queryKey: taskKeys.detail(id),
+        queryFn: () => taskApi.getTask(id),
+        enabled: !!id, // Запрос выполнится только если id существует
+    })
+}
+
+// Хук для создания задачи
+export const useCreateTask = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (taskData: createTask) => taskApi.createTask(taskData),
+        onSuccess: () => {
+            // Инвалидируем кэш задач после создания
+            queryClient.invalidateQueries({ queryKey: taskKeys.lists() })
+        },
+    })
+}
+
+// Хук для обновления задачи
+export const useUpdateTask = (id: number) => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: ({ id, taskData }: { id: number; taskData: updateTask }) => taskApi.updateTask(id, taskData),
+        onSuccess: (updatedUser) => {
+            // Обновляем конкретную задачу в кэше
+            queryClient.setQueryData(taskKeys.detail(id), updatedUser);
+            // Инвалидируем список задач
+            queryClient.invalidateQueries({ queryKey: taskKeys.lists() });
+        },
+    })
+}
+
+// Хук для обновления статуса задачи
+export const useUpdateTaskStatus = (id: number) => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: ({ id, taskStatus }: { id: number; taskStatus: {status: string} }) => taskApi.updateTaskStatus(id, taskStatus),
+        onSuccess: (updatedUser) => {
+            // Обновляем конкретную задачу в кэше
+            queryClient.setQueryData(taskKeys.detail(id), updatedUser);
+            // Инвалидируем список задач
+            queryClient.invalidateQueries({ queryKey: taskKeys.lists() });
+        },
+    })
+}
+
+
