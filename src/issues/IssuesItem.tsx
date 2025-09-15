19eba8e090ed8d887d5ae3b38086ecc3d59666b3
slec16 <sankopytov.76@mail.ru>
2025-09-16 02:48:06 +0300

    hotfix

--- src/issues/IssuesItem.tsx
+++ src/issues/IssuesItem.tsx
@@ -79,9 +79,9 @@ const IssuesItem = (props: IssuesItemProps) => {
             case 'Low':
                 return 'bg-gray-100 text-gray-700 border-gray-300 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600'
             case 'Medium':
-                return 'bg-yellow-100 text-yellow-700 border-yellow-300 dark:bg-yellow-800 dark:text-yellow-200 dark:border-yellow-600'
+                return 'bg-yellow-100 text-yellow-700 border-yellow-300 dark:bg-yellow-700/40 dark:text-yellow-200 dark:border-yellow-600'
             case 'High':
-                return 'bg-red-100 text-red-700 border-red-300 dark:bg-red-800 dark:text-red-200 dark:border-red-600'
+                return 'bg-red-100 text-red-700 border-red-300 dark:bg-red-700/40 dark:text-red-200 dark:border-red-600'
             default:
                 return 'bg-gray-100 text-gray-700 border-gray-300 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600'
         }
