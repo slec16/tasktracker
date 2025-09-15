c3cee03e702cb5ce19fd9ab8c57c787d18104e9c
kopytov <kopytov.aa@smtech.ru>
2025-09-15 14:57:35 +0300

    feat: drawer content

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,30 +1,102 @@
 // import { useState } from 'react'
-import { IconButton } from '@mui/material'
+import { Button, IconButton } from '@mui/material'
 import Drawer from '@mui/material/Drawer'
 import CloseIcon from '@mui/icons-material/Close'
 import TextField from '@mui/material/TextField'
-
+import Divider from '@mui/material/Divider'
+import MenuItem from '@mui/material/MenuItem'
+import { useNavigate } from 'react-router-dom'
 
 const DrawerContent = ({ drawerState, onCloseDrawer }: { drawerState: boolean, onCloseDrawer: () => void }) => {
 
+    const navigate = useNavigate()
+
+    const currencies = [
+        {
+            value: '1',
+            label: '1',
+        },
+        {
+            value: '2',
+            label: '2',
+        },
+        {
+            value: '3',
+            label: '3',
+        }
+    ];
+
 
     return (
-        <div className='flex flex-col px-3 py-3 gap-y-4 '>
+        <div className='flex flex-col px-3 pt-3 pb-10 gap-y-4 h-full'>
             <div className='flex flex-row justify-between items-center'>
                 <p className='text-2xl font-bold mr-2'>Создание/редактирование задачи</p>
                 <IconButton onClick={onCloseDrawer}><CloseIcon color='secondary' /></IconButton>
             </div>
             <TextField
-                label="Название" 
+                label="Название"
                 variant="standard"
-                
+
             />
             <TextField
                 label="Описание"
                 multiline
                 rows={4}
-                // variant="standard"
+            // variant="standard"
             />
+            <Divider />
+            <TextField
+                select
+                label="Проект"
+            >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <TextField
+                select
+                label="Приоритет"
+            >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <TextField
+                select
+                label="Статус"
+            >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <TextField
+                select
+                label="Исполнитель"
+            >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <div className='flex flex-row justify-between items-center mt-auto'>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        navigate('/boards')
+                        onCloseDrawer()
+                    }}
+                >
+                    Перейти на доску
+                </Button>
+                <Button variant="contained">Создать</Button>
+            </div>
         </div>
     )
 }
