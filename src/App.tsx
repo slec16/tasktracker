f83024113c187450d4feba4025a0c328c1622cb7
slec16 <sankopytov.76@mail.ru>
2025-09-16 00:17:52 +0300

    feat: QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,39 @@ import Board from './board/Board'
 import Boards from './boards/Boards'
 import Issues from './issues/Issues'
 import TaskDrawer from './components/Drawer'
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
 
 function App() {
 
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+
+    const queryClient = new QueryClient()
+
     return (
-        <ThemeProvider defaultTheme="light">
-            <div className='dark:text-slate-300 px-5'>
-                <Header />
-                <TabsRouter openTaskDrawer={() => setIsDrawerOpen(true)}/>
-                <div className='flex-1 flex flex-col overflow-hidden '>
-                    <Routes>
-                        <Route path="/" element={<Navigate to="/issues" replace />} />
-                        <Route path='/boards' element={<Boards />} />
-                        <Route path='/issues' element={<Issues />} />
-                        <Route path='/board/:id' element={<Board />} />
-                    </Routes>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider defaultTheme="light">
+                <div className='dark:text-slate-300 px-5'>
+                    <Header />
+                    <TabsRouter openTaskDrawer={() => setIsDrawerOpen(true)} />
+                    <div className='flex-1 flex flex-col overflow-hidden '>
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/issues" replace />} />
+                            <Route path='/boards' element={<Boards />} />
+                            <Route path='/issues' element={<Issues />} />
+                            <Route path='/board/:id' element={<Board />} />
+                        </Routes>
+                    </div>
+                    <TaskDrawer
+                        drawerState={isDrawerOpen}
+                        onCloseDrawer={() => setIsDrawerOpen(false)}
+                    />
                 </div>
-                <TaskDrawer 
-                    drawerState={isDrawerOpen}
-                    onCloseDrawer={() => setIsDrawerOpen(false)}
-                />
-            </div>
-        </ThemeProvider>
+            </ThemeProvider>
+        </QueryClientProvider>
     )
 }
 
